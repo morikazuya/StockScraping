6a6c01dad4805038fc8c648f61a14fe411d57b27
mori.kazuya <mori.kazuya2019@gmail.com>
2019-11-20 00:15:25 +0900

    nikkeiheikin Scraping

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.stock.Stock;
+
+@Controller
+public class IndexController {
+		
+	@RequestMapping("/index")
+	public String index(Model model) throws IOException {
+		
+			Document document = Jsoup.connect("https://kabutan.jp/stock/kabuka?code=0000&ashi=day&page=1").get();
+			Elements elements = document.select("table.stock_kabuka0 tbody");
+			
+			List<Stock> list = new ArrayList<Stock>();
+			
+			String str = null;
+			
+			for (Element element : elements) {
+				str = element.text();
+			}
+			
+			String hairetsu[] = str.split(" ");
+			Stock stock = new Stock();
+			stock.setToday(hairetsu[0]);
+			stock.setOpeningQuotation(hairetsu[1]);
+			stock.setHigh(hairetsu[2]);
+			stock.setLow(hairetsu[3]);
+			stock.setClosingQuotation(hairetsu[4]);
+			stock.setDayOverDayChanges(hairetsu[5]);
+			stock.setDayOverDayChangesPercent(hairetsu[6]);
+			stock.setTurnover(hairetsu[7]);
+			
+			list.add(stock);
+			
+		model.addAttribute("today", stock.getToday());
+		model.addAttribute("openingQuotation", stock.getOpeningQuotation());
+		model.addAttribute("high", stock.getHigh());
+		model.addAttribute("low", stock.getLow());
+		model.addAttribute("closingQuotation", stock.getClosingQuotation());
+		model.addAttribute("dayOverDayChanges", stock.getDayOverDayChanges());
+		model.addAttribute("dayOverDayChangesPercent", stock.getDayOverDayChangesPercent());
+		model.addAttribute("turnover", stock.getTurnover());
+
+		return "/index.html";
+	}
+}
---
+++ src/main/java/com/example/demo/stock/Stock.java
@@ -0,0 +1,63 @@
+package com.example.demo.stock;
+
+public class Stock {
+
+	private String today;//本日
+	private String openingQuotation;//始値
+	private String high;//高値
+	private String low;//安値
+	private String closingQuotation;//終値
+	private String dayOverDayChanges;//前日比
+	private String dayOverDayChangesPercent;//前日比％
+	private String turnover;//売買高(株)
+	
+	public String getToday() {
+		return today;
+	}
+	public void setToday(String today) {
+		this.today = today;
+	}
+	public String getOpeningQuotation() {
+		return openingQuotation;
+	}
+	public void setOpeningQuotation(String openingQuotation) {
+		this.openingQuotation = openingQuotation;
+	}
+	public String getHigh() {
+		return high;
+	}
+	public void setHigh(String high) {
+		this.high = high;
+	}
+	public String getLow() {
+		return low;
+	}
+	public void setLow(String low) {
+		this.low = low;
+	}
+	public String getClosingQuotation() {
+		return closingQuotation;
+	}
+	public void setClosingQuotation(String closingQuotation) {
+		this.closingQuotation = closingQuotation;
+	}
+	public String getDayOverDayChanges() {
+		return dayOverDayChanges;
+	}
+	public void setDayOverDayChanges(String dayOverDayChanges) {
+		this.dayOverDayChanges = dayOverDayChanges;
+	}
+	public String getDayOverDayChangesPercent() {
+		return dayOverDayChangesPercent;
+	}
+	public void setDayOverDayChangesPercent(String dayOverDayChangesPercent) {
+		this.dayOverDayChangesPercent = dayOverDayChangesPercent;
+	}
+	public String getTurnover() {
+		return turnover;
+	}
+	public void setTurnover(String turnover) {
+		this.turnover = turnover;
+	}
+	
+}
